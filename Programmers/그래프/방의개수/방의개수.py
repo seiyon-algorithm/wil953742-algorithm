458e22b2855e9a49e0487cbb49832d134a3c93aa
J045_김웅일 <66904178+wil953742@users.noreply.github.com>
2021-12-21 23:58:47 +0900

    백준: 2579 ( db )

---
+++ Programmers/그래프/방의개수/방의개수.py
@@ -0,0 +1,9 @@
+def solution(arrows):
+    answer = 0
+    return answer
+  
+
+sol = solution([6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0])
+print(sol) #3
+
+input = sys.stdin.readline
